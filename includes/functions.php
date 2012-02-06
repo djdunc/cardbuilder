30f5f615b0e9b95f8ad182b5a24fbf4728965998
Willem Van der Merwe <will.vdmerwe@sliderstudio.co.uk>
2012-02-06 13:57:49 +0000

    committing updated functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -3,11 +3,10 @@ function callAPI($query_params) {
 	$json_string = "";
     try {
     	$request = BASE_API.$query_params;
+        //create OAuth object using private key and secret
+        $oauth = new OAuth(PRIVATE_KEY, SECRET, OAUTH_SIG_METHOD_HMACSHA1);
         //parameters passed as data using array 
-        $oauth = $_SESSION['oauth'];
-        var_dump($request);
-        
-        $oauth->fetch($request ,array('id'=>1), OAUTH_HTTP_METHOD_GET);
+        $oauth->fetch($request ,array(), OAUTH_HTTP_METHOD_GET);
 
         //get JSON response string
         $json_string = $oauth->getLastResponse();   
@@ -17,9 +16,8 @@ function callAPI($query_params) {
 //        $php_assoc_array = json_decode($oauth->getLastResponse(), true);
 
     } catch(OAuthException $E) {
-        echo "Exception caught!\n";
-        echo "Response: ". $E->lastResponse . "\n";
-        echo "Response: ". $E->getMessage() . "\n";
+          $error = json_decode($oauth->getLastResponse());
+          echo $error->error->message;
     }
     return 	$json_string;
 }
