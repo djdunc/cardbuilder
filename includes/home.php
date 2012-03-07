e664c7d7446bda0475ed27ce49627ac81d4595a2
malicias <malicias@gmail.com>
2012-03-07 11:40:21 +0000

    testing thumb

--- includes/home.php
+++ includes/home.php
@@ -31,8 +31,7 @@ $(document).ready(function() {
                                     $tmp_front = ARUP_CARDS_URL.$card->card_front;
                                     $tmp_headers = @get_headers($tmp_front);
                                     if($tmp_headers[0] != 'HTTP/1.1 404 Not Found') {
-                                        $tmp_t = CroppedThumbnail($tmp_front,200,142);
-                                        $card_front = imagejpeg($tmp_t,'',500);
+                                        $card_front = showThumbnail($tmp_front,200,142);
                                     }
                                 } else{
                                     $card_front = UPLOADS_URL.'fronts/'.$card->card_front.'_t.jpg';
