4bd8d4b68b521fb076c80526db23291b25e0a5e0
malicias <malicias@gmail.com>
2012-03-07 11:03:19 +0000

    thumbnail images

--- includes/home.php
+++ includes/home.php
@@ -26,12 +26,18 @@ $(document).ready(function() {
 					    <?php if (isset($event_cards)&&count($event_cards)>0){?>
 						<?php $last_event_cards = array_reverse($event_cards); 
 						foreach (array_slice($last_event_cards, 0, 24) as $card) { 
-
-						if (isset($card->card_front)){ 
-						    $card_front = UPLOADS_URL."fronts/".$card->card_front."_t.jpg";
-						    if (!file_exists($card_front)){
-						        $card_front = $card->card_front;
-						    }
+						    if (isset($card->card_front)){
+						        if ($card->owner_user->id==1){
+                                    $card_front = ARUP_CARDS_URL.$card->card_front;
+                                } else{
+                                    $card_front = UPLOADS_URL.'fronts/'.$card->card_front.'.jpg';
+                                }
+                                $card_headers = @get_headers($card_front);
+                                if($card_headers[0] == 'HTTP/1.1 404 Not Found') {
+                                   // $card_front="false";
+                                }
+                            }
+						   
 						}
 						?>
 						<!-- GALLERY ITEM -->
