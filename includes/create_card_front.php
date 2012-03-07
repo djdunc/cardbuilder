926364555770bc2f30c930dbcbcd526ad2e7ec3a
malicias <malicias@gmail.com>
2012-03-07 11:22:20 +0000

    crop function in functions

--- includes/create_card_front.php
+++ includes/create_card_front.php
@@ -139,34 +139,6 @@ $saved_card_json = callAPI("card/put?id=".$card->id.'&card_front='.$filename);
 $saved_card = json_decode($saved_card_json);
 echo($saved_card->card_front);
 
-/////////////////resize & crop function
-function CroppedThumbnail($imgSrc,$thumbnail_width,$thumbnail_height) { //$imgSrc is a FILE - Returns an image resource.
-    //getting the image dimensions  
-    list($width_orig, $height_orig) = getimagesize($imgSrc);   
-    $myImage = imagecreatefromjpeg($imgSrc);
-    $ratio_orig = $width_orig/$height_orig;
-    
-    if ($thumbnail_width/$thumbnail_height > $ratio_orig) {
-       $new_height = $thumbnail_width/$ratio_orig;
-       $new_width = $thumbnail_width;
-    } else {
-       $new_width = $thumbnail_height*$ratio_orig;
-       $new_height = $thumbnail_height;
-    }
-    
-    $x_mid = $new_width/2;  //horizontal middle
-    $y_mid = $new_height/2; //vertical middle
-    
-    $process = imagecreatetruecolor(round($new_width), round($new_height)); 
-    
-    imagecopyresampled($process, $myImage, 0, 0, 0, 0, $new_width, $new_height, $width_orig, $height_orig);
-    $thumb = imagecreatetruecolor($thumbnail_width, $thumbnail_height); 
-    imagecopyresampled($thumb, $process, 0, 0, ($x_mid-($thumbnail_width/2)), ($y_mid-($thumbnail_height/2)), $thumbnail_width, $thumbnail_height, $thumbnail_width, $thumbnail_height);
-
-    imagedestroy($process);
-    imagedestroy($myImage);
-    return $thumb;
-}
 function ImageRectangleWithRoundedCorners(&$im, $x1, $y1, $x2, $y2, $radius, $color) {
     // draw rectangle without corners
     imagefilledrectangle($im, $x1+$radius, $y1, $x2-$radius, $y2, $color);
--- includes/upload.php
+++ includes/upload.php
@@ -52,32 +52,3 @@ if ((($_FILES['value']["type"] == "image/gif")
 	return die ("Please upload a valid gif, jpg or png under 200Kb");
 	//die();
 } 
-/////////////////resize & crop function
-function CroppedThumbnail($imgSrc,$thumbnail_width,$thumbnail_height) { //$imgSrc is a FILE - Returns an image resource.
-    //getting the image dimensions  
-    list($width_orig, $height_orig) = getimagesize($imgSrc);   
-    $myImage = imagecreatefromjpeg($imgSrc);
-    $ratio_orig = $width_orig/$height_orig;
-    
-    if ($thumbnail_width/$thumbnail_height > $ratio_orig) {
-       $new_height = $thumbnail_width/$ratio_orig;
-       $new_width = $thumbnail_width;
-    } else {
-       $new_width = $thumbnail_height*$ratio_orig;
-       $new_height = $thumbnail_height;
-    }
-    
-    $x_mid = $new_width/2;  //horizontal middle
-    $y_mid = $new_height/2; //vertical middle
-    
-    $process = imagecreatetruecolor(round($new_width), round($new_height)); 
-    
-    imagecopyresampled($process, $myImage, 0, 0, 0, 0, $new_width, $new_height, $width_orig, $height_orig);
-    $thumb = imagecreatetruecolor($thumbnail_width, $thumbnail_height); 
-    imagecopyresampled($thumb, $process, 0, 0, ($x_mid-($thumbnail_width/2)), ($y_mid-($thumbnail_height/2)), $thumbnail_width, $thumbnail_height, $thumbnail_width, $thumbnail_height);
-
-    imagedestroy($process);
-    imagedestroy($myImage);
-    return $thumb;
-}
-?>
