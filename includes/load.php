85af47e762fa4073c8b1d02a07c5d11da9f1be02
malicias <malicias@gmail.com>
2012-03-12 11:55:18 +0000

    only post, put, delete if user

--- includes/load.php
+++ includes/load.php
@@ -31,6 +31,13 @@ if ($_POST) {
         $org = json_decode($post_org);
         $options['organisation_id']=$org->id;
   }
+if($action=='post'||$action=='put'||$action=='delete') {
+  if(!is('user')) {
+  //must be logged in for POST|PUT|DELETE
+      echo "Not logged in; can't POST|PUT|DELETE";
+      die;
+	}
+}
   $myURL = $controller.'/'.$action.'?'; 
   $myURL .= http_build_query($options,'','&');
   $myjson = callAPI($myURL);
